e13a5c870eab223d8882f2ab9dc890cc1a1234b3
Ted Klingenberg <ted@doseofted.com>
2021-06-30 13:30:09 -0400

    Experimenting with file watcher to auto-update dependencies in dev

--- project/entrypoint.mjs
+++ project/entrypoint.mjs
@@ -1,5 +1,7 @@
 #!/usr/bin/env zx
 
+// import * as chokidar from "chokidar"
+
 /** Add color to console logs then echo. */
 function echo(strings, ...values) {
   let str = strings[0]
@@ -11,10 +13,23 @@ function echo(strings, ...values) {
 
 let prefix = "project"
 try {
-  prefix = (JSON.parse(await $`cat package.json`) || {}).name || prefix
+  prefix = JSON.parse(await $`cat package.json`)?.name ?? prefix
 } catch (error) {
   echo`Could not gather project name from package.json`
 }
 
+// if (process.env.NODE_ENV === "development") {
+//   const watcher = chokidar.watch('./node_modules')
+//   watcher.on('change', (path, stats) => {
+//     echo`File changed: ${path}`
+//     $`(yarn check --integrity && yarn check --verify-tree) || yarn --pure-lockfile`
+//   });
+// }
+
+// echo`Checking dependencies ...`
+// REFERENCE: https://gist.github.com/armand1m/b8061bcc9e8e9a5c1303854290c7d61e
+// await $`(yarn check --integrity && yarn check --verify-tree) || yarn --pure-lockfile`
+
+const args = process.argv.slice(3)
 echo`Starting app ...`
-$`yarn start`
+$`yarn ${args.length > 0 ? args.join(" ") : "start"}`
