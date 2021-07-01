6e7e5bc2be6dbdf6f39e8b255023316337a99a31
Ted Klingenberg <ted@doseofted.com>
2021-07-01 15:08:40 -0400

    Set up simple Fastify server in container

--- project/src/index.js
+++ project/src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello there.")
+import * as fastify from "fastify"
+
+const app = fastify({ logger: true })
+
+app.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+app.listen(3000, function (err, address) {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+  app.log.info(`server listening on ${address}`)
+})
