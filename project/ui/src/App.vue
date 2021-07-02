d95791e65d6414a0eb6cce8b6452f3fe8077abbe
Ted Klingenberg <ted@doseofted.com>
2021-07-02 12:03:48 -0400

    Vite project can now be served from Caddy (dev setup not working yet)

--- project/ui/src/App.vue
+++ project/ui/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld msg="Hello Ted." />
 </template>
 
 <script lang="ts">
