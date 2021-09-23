0b0dd7585dd109809fa5d2b46ecf977abf104ae3
Ted Klingenberg <ted@doseofted.com>
2021-09-23 12:48:51 -0400

    Added Tailwind to UI project

--- project/ui/src/main.ts
+++ project/ui/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue"
 import App from "./App.vue"
 import "@capacitor/core"
 import router from "./router"
+import "./style/index.scss"
 
 const app = createApp(App)
 app.use(router)
---
+++ project/ui/tailwind.config.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	mode: "jit",
+	purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	darkMode: "media", // or 'media' or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+}
