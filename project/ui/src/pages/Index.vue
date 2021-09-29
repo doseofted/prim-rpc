3df7bb356cee948c1837cad0597aa46fee0f707d
Ted Klingenberg <ted@doseofted.com>
2021-09-29 14:12:28 -0400

    Set up basic example with Paper.js

--- project/ui/src/pages/Index.vue
+++ project/ui/src/pages/Index.vue
@@ -1,22 +1,39 @@
 <template>
 	<div class="flex justify-center items-center h-screen">
-		<hello-world>Ted</hello-world>
+		<!-- <hello-world>Ted</hello-world> -->
+		<canvas
+			ref="canvas"
+			class="w-full h-full"
+			resize
+		/>
 	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue"
+<script lang="tsx">
+import { defineComponent, ref, onMounted } from "vue"
+import { Project, Shape, Point, Color, Rectangle } from "paper/dist/paper-core"
 
-export default defineComponent({})
+export default defineComponent({
+	setup() {
+		const canvas = ref<HTMLCanvasElement|null>(null)
+		onMounted(() => {
+			if (!canvas.value) { return }
+			const project = new Project(canvas.value)
+			const draw = () => {
+				project.clear()
+				const { width, height } = project.view.viewSize
+				const bg = new Shape.Rectangle(new Rectangle(new Point(0, 0), new Point(width, height)))
+				bg.fillColor = new Color("#000000")
+				const shape = new Shape.Circle(
+					new Point(width / 2, height / 2),
+					(width < height ? width : height) / 2
+				)
+				shape.fillColor = new Color("#ffffff")
+			}
+			project.view.on("resize", () => draw())
+			draw()
+		})
+		return { canvas }
+	}
+})
 </script>
-
-<style scoped>
-#app {
-  margin-top: 60px;
-  color: #2c3e50;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-</style>
