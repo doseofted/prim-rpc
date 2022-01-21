e9fd1ac14a0fb027700200ad9f0c4d1141888209
Ted Klingenberg <ted@doseofted.com>
2022-01-21 15:28:05 -0500

    Updated frontend with simple message that matches backend

---
+++ project/frontend/src/app.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HelloYou from './components/hello-you.vue'
+</script>
+
+<template>
+  <div class="greeting">
+    <hello-you name="you" />
+  </div>
+</template>
+
+<style>
+body { margin: 0; background-color: #dfdfdf; }
+
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
---
+++ project/frontend/src/components/hello-you.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ name: string }>()
+</script>
+
+<template>
+  <p>hello {{ name }}</p>
+</template>
+
+<style scoped>
+p {
+  font-family: sans-serif;
+  font-size: 14px;
+}
+</style>
