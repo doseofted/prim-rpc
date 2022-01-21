3edf1832351954d181b2404d7fdda7450653defb
Ted Klingenberg <ted@doseofted.com>
2022-01-21 15:21:50 -0500

    Frontend now works in container with monorepo project structure

---
+++ project/frontend/entrypoint.mjs
@@ -0,0 +1,28 @@
+#!/usr/bin/env zx
+import { createEcho, gatherPackageName, mode } from "../../misc/zx-utils.mjs"
+$.verbose = false
+
+const prefix = await gatherPackageName('frontend/package.json')
+const echo = createEcho(prefix)
+
+$.verbose = true
+let dev = new Promise(r => r()) // simple promise to resolve, if not in dev mode
+
+if (mode !== "production") {
+	echo`Running in ${mode} mode. Building in background ...`
+	dev = nothrow($`pnpm dev --filter="frontend"`) // wrap in `nothrow` since it's just a dev process
+}
+
+const args = process.argv.slice(3).join(" ")
+echo`Starting app in ${mode} mode ...`
+try {
+	const app = $`pnpm ${args || "preview"} --filter="frontend"`
+	process.on("SIGTERM", () => { // sigterm received from docker-compose
+		app.kill("SIGINT") // send interrupt, as if used interactively
+		if (dev) { dev.kill("SIGINT") } // same with dev, if in dev mode
+	})
+	await Promise.all([app, dev])
+} catch (p) {
+	echo`app exited, code ${p.exitCode}`
+	if (p.stderr) { echo`error: ${p.stderr}` }
+}
