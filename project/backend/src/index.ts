4cb341156e8dfc434c5691370cd35650c2c9bc32
Ted Klingenberg <ted@doseofted.com>
2022-02-02 01:01:20 -0500

    Added ability to use query string as additional paramter for use with JSON-LD, potentially

--- project/backend/src/index.ts
+++ project/backend/src/index.ts
@@ -7,11 +7,29 @@ const { you } = example
 
 const pluginTest: FastifyPluginAsync<{ example: object }> = async (fastify, { example }) => {
 	const prim = setupPrim(example)
-	fastify.route<{ Body: RpcCall }>({
+	fastify.route<{ Body: RpcCall, Querystring: unknown }>({
 		method: "POST",
 		url: "/",
-		handler: ({ body }, reply) => {
-			reply.send(prim(body))
+		handler: ({ body, query }, reply) => {
+			// NOTE: by using the query, some options could be passed in the URL
+			// for use with JSON-LD (they'll just be passed to parameters in RPC call
+			// so it doesn't matter if given in body or query, but if given in both
+			// then the body of the request should be prioritized for the sake of
+			// somewhat followng JSON-RPC).
+			// One useful place for this is numbering pages of data. A link could be
+			// clicked to bring user to another request
+			// TODO: the JSON-RPC ID given has to be unique enough to correlate request for
+			// additional pages back to original POST request, since all clicks on links
+			// to gather additional pages would be GET requests and would lose JSON-RPC
+			// parameters. It is always encouraged to send POST requests to the JSON-RPC
+			// endpoints but using GET requests to further gather linked data will make using this
+			// kind of API so much easier (for instance, I can literally click a link
+			// to go to the next page of data)
+			let params = typeof body.params === "object" ? body.params : {}
+			if (typeof query === "object") { params = { ...params, ...query } }
+			// TODO: consider using lodash's merge or "defu" library for mergng query into params of RPC call
+			console.log(params)
+			reply.send(prim({ ...body, params }))
 		}
 	})
 }
