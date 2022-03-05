d652fbbe178eae9967346ca1a2f62b72238ddb52
Ted Klingenberg <ted@doseofted.com>
2022-03-05 16:05:26 -0500

    Server is better separated from Prim and using separate plugin

--- libraries/packages/prim-plugins/src/index.ts
+++ libraries/packages/prim-plugins/src/index.ts
@@ -1,37 +1,28 @@
-import { RpcCall, createPrimServer } from "prim"
+import { RpcCall, createPrimServer, PrimOptions } from "prim"
 import type { FastifyPluginAsync } from "fastify"
 
-export function primFasifyPlugin<T extends Record<V, T[V]>, V extends keyof T = keyof T>() {
-	const plugin: FastifyPluginAsync<{ module: T, prefix?: string }> = async (fastify, options) => {
-		const { prefix = "/prim" } = options
-		const prim = createPrimServer({ server: true }, options.module)
-		fastify.route<{ Body: RpcCall, Params: { method?: string } }>({
-			method: ["POST", "GET"],
-			url: `${prefix}`,
-			handler: async ({ body, url: path, query }, reply) => {
-				const response = await prim({ path, prefix, body, query })
-				reply.send(response)
-			}
-		})
-	}
-	return plugin
+interface PrimFastifyOptions {
+	// Prims-specific options
+	options?: PrimOptions
+	module: unknown
+	// Fastify-specific options
+	prefix?: string
 }
 
-/* export function primFasifyPlugin<T extends Record<V, T[V]>, V extends keyof T = keyof T>() {
-	return fp<{ module: T, prefix?: string }>(async (fastify, options) => {
-		const { prefix = "/prim" } = options
-		const prim = createPrimServer({ server: true }, options.module)
-		fastify.route<{ Body: RpcCall, Params: { method?: string } }>({
-			method: ["POST", "GET"],
-			url: `${prefix}`,
-			handler: async ({ body, url: path, query }, reply) => {
-				const response = await prim({ path, prefix, body, query })
-				reply.send(response)
-			}
-		})
+export const primFasifyPlugin: FastifyPluginAsync<PrimFastifyOptions> = async (fastify, options) => {
+	const { prefix = "/prim", options: primOptions = {}, module: givenModule } = options
+	primOptions.server = true // this is true since it is being used from a server framework
+	const prim = createPrimServer(primOptions, givenModule)
+	fastify.route<{ Body: RpcCall, Params: { method?: string } }>({
+		method: ["POST", "GET"],
+		url: `${prefix}`,
+		handler: async ({ body, url: path, query }, reply) => {
+			const response = await prim({ path, prefix, body, query })
+			reply.send(response)
+		}
 	})
-} */
+}
 
-export function primExpressPlugin(_req: Request, _res: Response, _next: () => void) {
+export const primExpressPlugin = (_req: Request, _res: Response, _next: () => void) => {
 	// ...
 }
--- project/backend/src/index.ts
+++ project/backend/src/index.ts
@@ -1,27 +1,10 @@
 import Fastify from "fastify"
 import Cors from "fastify-cors"
-// import fp from "fastify-plugin"
 import * as example from "example"
-// import { createPrimServer, RpcCall } from "prim"
 import { primFasifyPlugin } from "prim-plugins"
 
-/* function primFasifyPlugin<T extends Record<V, T[V]>, V extends keyof T = keyof T>() {
-	return fp<{ module: T, prefix?: string }>(async (fastify, options) => {
-		const { prefix = "/prim" } = options
-		const prim = createPrimServer({ server: true }, options.module)
-		fastify.route<{ Body: RpcCall, Params: { method?: string } }>({
-			method: ["POST", "GET"],
-			url: `${prefix}`,
-			handler: async ({ body, url: path, query }, reply) => {
-				const response = await prim({ path, prefix, body, query })
-				reply.send(response)
-			}
-		})
-	})
-} */
-
 const fastify = Fastify({ logger: true })
-fastify.register(primFasifyPlugin(), { module: example })
+fastify.register(primFasifyPlugin, { module: example })
 fastify.register(Cors, { origin: `https://${process.env.HOST}` })
 
 fastify.listen(3001, "0.0.0.0", function (err) {
