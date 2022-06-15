7b1a645ff3e20eca5a09184d65aae85243985c07
Ted Klingenberg <ted@doseofted.com>
2022-06-15 15:07:37 -0400

    Tried using Parcel for building Vue components as library but it's looking like Vite still might be the best option for frontend

---
+++ libs/interface/src/index.ts
@@ -0,0 +1,3 @@
+import Test from "./test.vue"
+
+export { Test }
---
+++ libs/interface/src/test.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { ref } from "vue"
+const test = ref("Ted")
+</script>
+
+<template>
+  <div :style="{ fontSize: '40px' }">
+    {{ test }}
+  </div>
+</template>
