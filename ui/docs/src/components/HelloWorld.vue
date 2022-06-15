6a42189b33f15342893e10fc4e393a656cbb75c3
Ted Klingenberg <ted@doseofted.com>
2022-06-15 17:52:47 -0400

    Added a future component library project in "ui" folder instead of existing "libs" folder because Parcel can't yet ignore files with glob patterns

---
+++ ui/docs/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <button type="button" @click="count++">
+    <template v-for="i of count" :key="i">
+    {{ msg }}
+    </template>
+  </button>
+</template>
