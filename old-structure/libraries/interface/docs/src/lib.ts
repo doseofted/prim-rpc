267920e8596540d0f4e30df1681630bd4691295c
Ted Klingenberg <ted@doseofted.com>
2022-06-15 20:12:24 -0400

    Removed old interface library (was more of a playground for docs anyway), moved some utilities, using code-workspace again in VS Code

--- old-structure/libraries/interface/docs/src/lib.ts
+++
@@ -1,8 +0,0 @@
-import PrimHello from "./components/PrimHello.vue"
-import { generateTsDocs } from "./playground"
-
-// console.table(generateJsDocs())
-console.clear()
-console.log(JSON.stringify(generateTsDocs(), null, "\t"))
-
-export { PrimHello }
