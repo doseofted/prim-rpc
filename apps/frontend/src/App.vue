83ae5c25887b039c80daebb154484e90a90f6773
Ted Klingenberg <ted@doseofted.com>
2022-06-16 20:34:11 -0400

    Added back frontend test for Prim (not working since Docker configs not added back yet)

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue"
+import TestOnly from "./components/TestOnly.vue"
 </script>
 
 <template>
-  <HelloWorld msg="Prim" />
+  <TestOnly />
 </template>
