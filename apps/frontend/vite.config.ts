15e45a5494b777a83d8ea9fc5ea0cd86fadcfb18
Ted Klingenberg <ted@doseofted.com>
2022-06-16 20:21:44 -0400

    Added a frontend project (by copying docs/ui), to replace old frontend project soon

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,34 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite"
+import { resolve as resolvePath } from "node:path"
+import vue from "@vitejs/plugin-vue"
+import dts from "vite-plugin-dts"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue(), dts()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
+	build: {
+		lib: {
+			formats: ["es", "umd"],
+			entry: resolvePath(__dirname, "src/lib.ts"),
+			name: "lib",
+			fileName: (format) => `lib.${format}.js`,
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ["vue"],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					vue: "Vue",
+				},
+			},
+		},
+	},
+})
