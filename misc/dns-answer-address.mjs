e2192ca022a8b1718cb7eb32d25f480d67266d15
Ted Klingenberg <ted@doseofted.com>
2022-07-11 12:40:59 -0400

    Tested out DNS address script on Mac and made changes to support it

--- misc/dns-answer-address.mjs
+++ misc/dns-answer-address.mjs
@@ -8,16 +8,21 @@ $.silent = true
  * This is intended to be used with `task dev:dns` of this project.
  * The IP address returned is used for resolving DNS requests to that IP address.
  *
+ * Currently IPv4 addresses are preferred but this could be changed if needed.
+ *
  * @param {string} startsWith Portion of requested IP address
  * @returns Requested IP address
  */
 export function ipAddressForCoreDns(startsWith = "") {
 	const networks = networkInterfaces()
-	delete networks.lo
+	const external = Object.values(networks).flat().filter(n => !n.internal)
 	if (!startsWith) {
-		Object.values(networks).flat()[0]?.address ?? ""
+		const firstIpv4 = external.find(n => n.family === "IPv4")
+		if (firstIpv4) { return firstIpv4?.address ?? "" }
+		const firstAnyFamily = external[0]?.address ?? ""
+		return firstAnyFamily
 	}
-	return Object.values(networks).flat().find(n => n.address.startsWith(startsWith))?.address ?? ""
+	return external.find(n => n.address.startsWith(startsWith))?.address ?? ""
 }
 
 if (argv.help || argv.h) {
