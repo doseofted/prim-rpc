5c7a05c22ccea78e67dc479427cf337203dbd749
Ted Klingenberg <ted@doseofted.com>
2022-07-13 16:07:48 -0400

    Moved vitest configuration into its own file since types are not working in vite.config.ts

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,12 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-	},
 })
