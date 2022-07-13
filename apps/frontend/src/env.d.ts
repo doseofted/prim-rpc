1152fcf81904fa80b08e8740be0c6bbc14bd46a0
Ted Klingenberg <ted@doseofted.com>
2022-07-13 16:09:31 -0400

    Revert "Moved vitest configuration into its own file since types are not working in vite.config.ts"
    This reverts commit 5c7a05c22ccea78e67dc479427cf337203dbd749.

--- apps/frontend/src/env.d.ts
+++ apps/frontend/src/env.d.ts
@@ -1,5 +1,4 @@
 /// <reference types="vite/client" />
-/// <reference types="vitest" />
 
 declare module "*.vue" {
 	import type { DefineComponent } from "vue"
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
 })
