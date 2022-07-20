7625304e6a9cc91425b51a3637cce6c0c11fa8a2
Ted Klingenberg <ted@doseofted.com>
2022-07-20 10:39:49 -0400

    Removed preload links (for now, since it was broken) and replaced h3/listhen with fastify

--- apps/frontend/server.mjs
+++ apps/frontend/server.mjs
@@ -1,28 +1,30 @@
-
+// @ts-check
 import path from "node:path"
 import { fileURLToPath } from "node:url"
 import { readFileSync } from "node:fs"
-import { createApp, createRouter } from "h3"
-import { listen } from "listhen"
+import Fastify from "fastify"
+import middie from "@fastify/middie"
 
 const isTest = process.env.NODE_ENV === "test" || !!process.env.VITE_TEST_BUILD
 const isProd = process.env.NODE_ENV === "production"
-const root = process.cwd()
+
+/** @type {(path: string) => string} */
+function relativeToProject (relativePath) {
+	const projectDirectory = path.dirname(fileURLToPath(import.meta.url))
+	return path.resolve(projectDirectory, relativePath)
+}
 
 export async function createServer() {
-	const dirname = path.dirname(fileURLToPath(import.meta.url))
-	const resolvePath = p => path.resolve(dirname, p)
-	const indexProd = isProd
-		? readFileSync(resolvePath("dist/client/index.html"), "utf-8")
-		: ""
-	const { default: manifest } = isProd ? (await import("./dist/client/ssr-manifest.json")) : {}
-	const app = createApp()
-	/** @type {import('vite').ViteDevServer} */
-	let vite
+	const htmlProductionLocation = relativeToProject("dist/client/index.html")
+	const htmlProductionUse = isProd ? readFileSync(htmlProductionLocation, "utf-8") : ""
+	const { default: ssrManifest } = isProd ? (await import("./dist/client/ssr-manifest.json")) : {}
+	const app = Fastify()
+	await app.register(middie)
+	/** @type {import('vite').ViteDevServer|null} */ let vite = null
 	if (!isProd) {
 		const { default: viteImport } = await import("vite")
 		vite = await viteImport.createServer({
-			root,
+			root: process.cwd(),
 			logLevel: isTest ? "error" : "info",
 			server: {
 				middlewareMode: true,
@@ -31,48 +33,42 @@ export async function createServer() {
 					interval: 100,
 				},
 			},
-			appType: "custom",
 		})
 		app.use(vite.middlewares)
 	} else {
 		const { default: serveStatic } = await import("serve-static")
-		app.use("/", serveStatic(resolvePath("dist/client"), { index: false }))
+		app.use("/", serveStatic(relativeToProject("dist/client"), { index: false }))
 	}
-
-	const router = createRouter()
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	router.get("/", async (req, res, next) => {
+	app.get("/", async (request, reply) => {
 		try {
-			const url = req.originalUrl
+			const url = request.url
 			let template, render
 			if (!isProd) {
-				template = readFileSync(resolvePath("index.html"), "utf-8")
-				template = await vite.transformIndexHtml(url, template)
-				render = (await vite.ssrLoadModule("/src/entry.server.ts")).render
+				template = readFileSync(relativeToProject("index.html"), "utf-8")
+				template = await vite?.transformIndexHtml(url, template)
+				render = (await vite?.ssrLoadModule("/src/entry.server.ts"))?.render
 			} else {
-				template = indexProd
-				render = (await import("./dist/server/entry.server.js")).render
+				template = htmlProductionUse
+				render = (await import("./dist/server/entry.server.js"))?.render
 			}
-			const [appHtml, preloadLinks] = await render(url, manifest)
-			const html = template
-				.replace("<!--preload-links-->", preloadLinks)
-				.replace("<!--ssr-outlet-->", appHtml)
-			res.setHeader("Content-Type", "text/html")
-			res.statusCode = 200
-			res.end(html)
+			const appHtml = await render(url, ssrManifest)
+			const html = template?.replace("<!--ssr-outlet-->", appHtml)
+			reply.header("Content-Type", "text/html")
+			reply.status(200)
+			reply.send(html)
 		} catch (e) {
 			vite && vite.ssrFixStacktrace(e)
 			console.log(e.stack)
-			res.statusCode = 500
-			res.end(e.stack)
+			reply.status(500)
+			reply.send(e.stack)
 		}
 	})
-	app.use(router)
 
 	return { app, vite }
 }
 
 if (!isTest) {
 	const { app } = await createServer()
-	listen(app, { port: 6173 })
+	app.listen({ port: 6173, host: "0.0.0.0" })
 }
--- apps/frontend/src/entry.server.ts
+++ apps/frontend/src/entry.server.ts
@@ -1,4 +1,4 @@
-import { basename, extname } from "node:path"
+// import { basename, extname } from "node:path"
 import { RouteLocationRaw } from "vue-router"
 import { renderToString, SSRContext } from "vue/server-renderer"
 import { createApp } from "./main"
@@ -9,75 +9,12 @@ interface ManifestStructureGuess {
 
 export async function render(
 	url: RouteLocationRaw,
-	manifest: ManifestStructureGuess,
-): Promise<[html: string, preload: string]> {
+	_manifest: ManifestStructureGuess,
+): Promise<string> {
 	const { app, router } = createApp()
 	void router.push(url)
 	await router.isReady()
 	const ctx: SSRContext & { modules?: Set<string> } = {}
 	const html = await renderToString(app, ctx)
-	const preloadLinks = renderPreloadLinks(ctx.modules, manifest)
-	return [html, preloadLinks]
-}
-
-/**
- * Based on Vue example in Vite repo.
- * 
- * LINK https://github.com/vitejs/vite/blob/d12d469dfb8d7466615b085aac0d461ff18a4465/playground/ssr-vue/src/entry-server.js#L26
- */
-function renderPreloadLinks(modules?: Set<string>, manifest?: ManifestStructureGuess) {
-	const seen = new Set()
-	if (!modules) { return "" }
-	const modulesList = Array.from(modules)
-	const links = modulesList
-		.flatMap((id) => {
-			const files = manifest?.[id]
-			if (!Array.isArray(files)) { return [] }
-			return files
-		}).flatMap((file) => {
-			const links: string[] = []
-			if (seen.has(file)) { return [] }
-			seen.add(file)
-			const filename = basename(file)
-			const depFiles = manifest?.[filename]
-			if (!depFiles) { return [] }
-			for (const depFile of depFiles) {
-				links.push(renderPreloadLink(depFile))
-				seen.add(depFile)
-			}
-			links.push(renderPreloadLink(file))
-			return links
-		})
-		.join("")
-	return links
-}
-
-type LinkAttrs = "crossorigin"|"href"|"media"|"referrerpolicy"|"rel"|"type"|"as"
-const createLinkTag = (props: Partial<Record<LinkAttrs, string|true>>) =>
-	`<link ${Object.entries(props)
-		.map(([attr, val]) => val === true ? [attr] : [attr, val])
-		.map(p => p.join("="))
-		.join(" ")}>`
-function renderPreloadLink (href: string) {
-	const extension = extname(href).replace(".", "")
-	const mappedType = (given: string) => ({
-		jpg: "jpeg",
-	}[given] ?? given)
-	const rel = "preload"
-	switch (extension) {
-		case "js":
-			return createLinkTag({ href, rel: "modulepreload", crossorigin: true })
-		case "css":
-			return createLinkTag({ href, rel: "stylesheet" })
-		case "woff":
-		case "woff2":
-			return createLinkTag({ href, rel, as: "font", type: `font/${mappedType(extension)}`, crossorigin: true })
-		case "gif":
-		case "png":
-		case "jpeg":
-		case "jpg:":
-			return createLinkTag({ href, rel, as: "image", type: `image/${mappedType(extension)}`})
-		default:
-			return ""
-	}
+	return html
 }
