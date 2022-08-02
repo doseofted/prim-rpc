966940e83c186a81c91077e50191fbe2f7d5cdc1
Ted Klingenberg <ted@doseofted.com>
2022-08-02 11:49:01 -0400

    Playing around with idea of only building once file changes (since turborepo approach is memory/cpu intensive), WIP

--- build-deps.mjs
+++ build-deps.mjs
@@ -1,5 +1,33 @@
 #!/usr/bin/env zx
+// @ts-check
 import { $ } from "zx"
+// import { watch } from "chokidar"
+// import path from "node:path"
+// import { Parcel, createWorkerFarm } from "@parcel/core"
+// import { build } from "vite"
+// import { fileURLToPath } from "node:url"
+// let workerFarm = createWorkerFarm()
+// /** @param {string} entry */
+// const parcelBuild = (entry) => new Parcel({
+// 	workerFarm,
+// 	entries: [entry], 
+// 	defaultConfig: "@parcel/config-default",
+// 	additionalReporters: [
+// 		{
+// 			packageName: "@parcel/reporter-cli",
+// 			resolveFrom: fileURLToPath(import.meta.url),
+// 		},
+// 	],
+// }).run()
+// /** @param {string} root */
+// const viteBuild = (root) => {
+// 	return build({ root, configFile: "" })
+// }
+// const libsPath = path.join(__dirname, "libs")
+// watch(libsPath, { ignored: /node_modules/ })
+// 	.on("all", (e, p) => {
+// 		console.log(e, path.relative(libsPath, p).split(path.sep)[0])
+// 	})
 
 /** @type {{ [processName: string]: import("zx").ProcessPromise }} */
 const processes = {}
