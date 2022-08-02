7f21b3d239f43b2b3508e5bd45d768e73ff82167
Ted Klingenberg <ted@doseofted.com>
2022-08-02 12:35:37 -0400

    Using chokidar and turborepo for building to hopefully reduce cpu/mem usage (between turborepo cache and parcel cache, this should help quite a bit)

--- build-deps.mjs
+++ build-deps.mjs
@@ -1,8 +1,9 @@
 #!/usr/bin/env zx
 // @ts-check
 import { $ } from "zx"
-// import { watch } from "chokidar"
-// import path from "node:path"
+import { watch } from "chokidar"
+import path from "node:path"
+// import fs from "node:fs/promises"
 // import { Parcel, createWorkerFarm } from "@parcel/core"
 // import { build } from "vite"
 // import { fileURLToPath } from "node:url"
@@ -23,12 +24,19 @@ import { $ } from "zx"
 // const viteBuild = (root) => {
 // 	return build({ root, configFile: "" })
 // }
-// const libsPath = path.join(__dirname, "libs")
-// watch(libsPath, { ignored: /node_modules/ })
-// 	.on("all", (e, p) => {
-// 		console.log(e, path.relative(libsPath, p).split(path.sep)[0])
-// 	})
-
+const libsPath = path.join(__dirname, "libs")
+watch(libsPath, { ignored: /node_modules/, ignoreInitial: true })
+	.on("all", async (e, p) => {
+		const relativePath = path.relative(libsPath, p)
+		const projectPath = relativePath.split(path.sep)[0]
+		const filter = "." + path.sep + path.join("libs", projectPath)
+		const createProc = () => {
+			const proc = $`pnpm build --filter=${filter}`
+			void (async () => { await proc; delete processes[projectPath] })()
+			return proc
+		}
+		processes[projectPath] = processes[projectPath] || createProc()
+	})
 /** @type {{ [processName: string]: import("zx").ProcessPromise }} */
 const processes = {}
 process.on("SIGTERM", () => { // sigterm received from docker-compose
@@ -39,7 +47,7 @@ process.on("SIGTERM", () => { // sigterm received from docker-compose
 
 const inDevelopment = process.env.NODE_ENV === "development"
 if (inDevelopment) {
-	processes.buildLibs = $`pnpm watch-libs`
+	// processes.buildLibs = $`pnpm watch-libs`
 } else {
 	processes.tail = $`tail -f /dev/null`
 }
