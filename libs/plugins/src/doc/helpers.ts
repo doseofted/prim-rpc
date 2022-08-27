be979a569c18940441b9e60b850ebb9bc19996cd
Ted Klingenberg <ted@doseofted.com>
2022-08-27 13:35:36 -0400

    Removed attempted interface for TypeDoc (module already provides JSON output types)

--- libs/plugins/src/doc/helpers.ts
+++ libs/plugins/src/doc/helpers.ts
@@ -12,30 +12,3 @@ export function parseComment (comment: JSONOutput.Comment, tagged?: string): str
 		: comment?.summary?.map(sum => sum.text).reduce((p, n) => p + n, "")
 	return output ?? ""
 }
-
-export function parseModule (docs: JSONOutput.ProjectReflection) {
-	const { name: module } = docs
-	const methodList = findChildrenOfType("Functions", docs)
-		.map(methodGiven => {
-			const { name: method, signatures } = methodGiven
-			const overloads  = signatures
-				.filter(sig => sig.kindString === "Call signature")
-				.map(signature => {
-					const comment = parseComment(signature.comment)
-					const returns = parseComment(signature.comment, "@returns")
-					const params = signature.parameters.map(param => {
-						const name = param.name
-						const comment = parseComment(param.comment)
-						const type = param?.type.type // TODO: handle reflection
-						return { name, comment, type }
-					})
-					return { comment, returns, params }
-				})
-			return { method, overloads }
-		})
-	const methods: { [method: string]: typeof methodList["0"]["overloads"]} = {}
-	for (const method of methodList) {
-		methods[method.method] = method.overloads
-	}
-	return { module, methods, submodules: {} }
-}
--- libs/plugins/src/doc/index.ts
+++ libs/plugins/src/doc/index.ts
@@ -1,2 +1,30 @@
 // FIXME: I'm writing logic for docs in the prim-plugins module now but this should move into its own module eventually
-export { parseModule } from "./helpers"
+import { JSONOutput } from "typedoc"
+import { findChildrenOfType, parseComment } from "./helpers"
+
+export function parseModule (docs: JSONOutput.ProjectReflection) {
+	const { name: module } = docs
+	const methodList = findChildrenOfType("Functions", docs)
+		.map(methodGiven => {
+			const { name: method, signatures } = methodGiven
+			const overloads  = signatures
+				.filter(sig => sig.kindString === "Call signature")
+				.map(signature => {
+					const comment = parseComment(signature.comment)
+					const returns = parseComment(signature.comment, "@returns")
+					const params = signature.parameters.map(param => {
+						const name = param.name
+						const comment = parseComment(param.comment)
+						const type = param?.type.type // TODO: handle reflection
+						return { name, comment, type }
+					})
+					return { comment, returns, params }
+				})
+			return { method, overloads }
+		})
+	const methods: { [method: string]: typeof methodList["0"]["overloads"]} = {}
+	for (const method of methodList) {
+		methods[method.method] = method.overloads
+	}
+	return { module, methods, submodules: {} }
+}
