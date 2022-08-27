c5e852e9f18359fc51a61c50dfc4bd829ac2b63d
Ted Klingenberg <ted@doseofted.com>
2022-08-27 18:21:37 -0400

    Cleaned up new possible interface

--- libs/plugins/src/doc/interfaces.ts
+++ libs/plugins/src/doc/interfaces.ts
@@ -1,73 +1,17 @@
-// /** test */
-// type TypeRef = string
-// interface Documented {
-// 	/** Comment for given type using Markdown */
-// 	comment: string
-// 	/** A unique identifier to reference this object */
-// 	id: number
-// 	/** A categorization for type as recognized by documentation */
-// 	categorization: "Method"|"Param"|"Returns"|"Thrown"|"Module"
-// 	type: TypeRef
-// }
-// interface Returns extends Documented {
-// 	categorization: "Returns"
-// }
-
-// interface Param extends Documented {
-// 	categorization: "Param"
-// }
-
-// interface MethodOverload {
-// 	comment: string
-// 	params: Param[]
-// 	result: Returns
-// 	throws: Returns
-// }
-
-// export interface PrimRpcModule {
-// 	[name: string]: {
-// 		type: "Method"|"Module"
-// 		module?: PrimRpcModule
-// 		method?: MethodOverload[]
-// 	}
-// }
-
-// export interface TypeDocumented {
-// 	/** Either a primitive value or an object.  */
-// 	type: string
-// 	ref: string
-// 	object?: PrimObjectType
-// 	array?: TypeDocumented[]
-// }
-
-// export interface PrimObjectType {
-// 	[name: string]: TypeDocumented
-// }
-
-// export interface PrimRpcDocs {
-// 	/** Methods exported from modules that can be called */
-// 	module: PrimRpcModule
-// 	/** Types referenced from RPC calls */
-// 	types: PrimObjectType
-// }
-
-
-// TRY 2
-
-interface Param {
+interface RpcParam {
 	/** Parameter name */
 	name: string
 	/** A type as defined on Prim Docs's root object */
-	type: TypeRef
+	type: PrimDocRootRef
 	/** Identifier used to access name given by `.type` */
 	id: string
 	/** Comment for parameter value */
 	comment: string
 }
 
-interface Returned {
+interface RpcReturned {
 	/** A type as defined on Prim Docs's root object */
-	type: TypeRef
+	type: PrimDocRootRef
 	/** Identifier used to access name given by `.type` */
 	id: string
 	/** Comment for return or thrown value */
@@ -80,17 +24,17 @@ interface RpcMethodDocs {
 	/** Comment given for the method */
 	comment: string
 	/** Parameters expected by this overload of the method */
-	params: Param[],
+	params: RpcParam[],
 	/** Return type of the method */
-	returns: Returned,
+	returns: RpcReturned,
 	/** The type of error that is thrown directly by (and only by) this method */
-	throws: Returned
+	throws: RpcReturned
 }
 
 interface RpcTypeDocs {
 	/** Value of `intrinsic` will determine what property on object should be accessed */
 	intrinsic: "type"|"object"|"array"
-	/** If not an object or array, `.type` is the type of parameter expected native, to the language */
+	/** If not an object or array, `.type` is the type of parameter expected, native to the language */
 	type?: string
 	/** If given type has properties, they will be listed under `.object` */
 	object?: {
@@ -100,7 +44,7 @@ interface RpcTypeDocs {
 	array?: RpcTypeDocs[],
 }
 
-interface RpcDocReferences {
+interface PrimRpcDocReferences {
 	/** Methods grouped by a generated identifier. Objects with call signatures */
 	method: {
 		[id: string]: RpcMethodDocs
@@ -111,22 +55,22 @@ interface RpcDocReferences {
 	}
 }
 /** A type that is recognized by `PrimDocReferences` and has values */
-type TypeRef = keyof RpcDocReferences
+type PrimDocRootRef = keyof PrimRpcDocReferences
 
-interface Shape {
+interface PrimRpcModuleShape {
 	/** Either a method name or property used to access a submodule */
 	[property: string]: {
 		/** Type used to access Prim Docs' properties and find a specific ID */
-		type: TypeRef
+		type: PrimDocRootRef
 		/** ID used to reference root property named with value of `.type` on Prim Docs root object */
 		id: string
 		/** If given option has properties, those will be nested under `.shape` */
-		shape: Shape
+		shape: PrimRpcModuleShape
 	}
 }
 
 /** Prim RPC documentation, generated from a TypeDoc export */
-export interface RpcDocs extends RpcDocReferences {
+export interface PrimRpcDocs extends PrimRpcDocReferences {
 	/** The shape of the module including direct methods and any nested modules */
-	shape: Shape
+	shape: PrimRpcModuleShape
 }
