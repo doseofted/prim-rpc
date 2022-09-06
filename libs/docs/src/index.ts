15082098bafeac77550fd8431714d0f5bb7459c4
Ted Klingenberg <ted@doseofted.com>
2022-09-06 08:15:55 -0400

    Replaced old interfaces with simplified interfaces for doc generation (and added example structure to index.ts of docs module)

--- libs/docs/src/index.ts
+++ libs/docs/src/index.ts
@@ -1,7 +1,6 @@
 import type { JSONOutput } from "typedoc"
 import { getDeclarationPropReflected, isTypeDoc, parseComment } from "./helpers"
-import { PrimRpcDocs, PrimMethod, PrimModule, PrimModuleStructure } from "./type"
-
+import { PrimRpcDocs, PrimMethod, PrimModule, PrimModuleStructure } from "./interfaces"
 
 function handleType (given: JSONOutput.DeclarationReflection) {
 	const name = given.name
@@ -38,23 +37,23 @@ function handleFunction (given: JSONOutput.DeclarationReflection, path: string[]
  * @param given - Some level of TSDoc declaration output
  * @param path - The path given given at level (for recursion)
  */
-function navigateProperties (given: JSONOutput.DeclarationReflection, path: string[] = []) {
+function navigateProperties (given: JSONOutput.DeclarationReflection, docs: PrimRpcDocs, path: string[] = []) {
 	given.children.forEach(child => {
 		const hasSignature = getDeclarationPropReflected(child, "signatures")
 		const hasChildren = getDeclarationPropReflected(child, "children")
 		if (hasSignature.value) {
 			handleFunction(hasSignature.given, path)
 		} else if (hasChildren.value) {
-			navigateProperties(hasChildren.reflected ?? hasChildren.given, path.concat(child.name))
+			navigateProperties(hasChildren.reflected ?? hasChildren.given, docs, path.concat(child.name))
 		}
 	})
 }
 
-function addToDocs (docs?: PrimRpcDocs) {
-	const documentation = docs ?? {
-		methods: [],
-		modules: [],
-	}
+
+function addMethodToDocs(docs: PrimRpcDocs, method: PrimMethod) {
+	const documentation = docs
+	// TODO: add to docs structure based on given `method.path`
+	docs.methods.push(method)
 }
 
 /**
@@ -62,28 +61,27 @@ function addToDocs (docs?: PrimRpcDocs) {
  * for Prim Docs UI to create a documentation page. A simpler version of TypeDoc
  * intended only for documenting RPC calls.
  *
- * @param docs - Output of TypeDoc documentation for module used with Prim
+ * @param given - Output of TypeDoc documentation for module used with Prim
  * @returns Prim-specific documentation for RPC calls
  */
-export function createDocsForModule(docs: unknown): PrimRpcDocs {
-	if (!isTypeDoc(docs)) {
+export function createDocsForModule(given: unknown): PrimRpcDocs {
+	if (!isTypeDoc(given)) {
 		throw new Error("Given documentation was not understood as TypeDoc format")
 	}
-	const methods: PrimMethod[] = []
+	// const methods: PrimMethod[] = []
 	const modules: PrimModule[] = []
-	const newProp = {
-		name: docs.name,
-		comment: parseComment(docs.comment),
-		flags: {},
+	const index = modules.push({
+		name: given.name,
+		comment: parseComment(given.comment),
+		flags: given.flags,
 		path: "",
-	}
-	const index = modules.push(newProp) - 1
+	}) - 1
 	const structure: PrimModuleStructure = {
 		docs: ["modules", index],
 		props: {},
 	}
-	const generated: PrimRpcDocs = { ...structure, methods: [], modules: [] }
-	navigateProperties(docs)
+	const docs: PrimRpcDocs = { ...structure, modules, methods: [] }
+	navigateProperties(given, docs)
 	return {
 		props: {
 			test: {
--- libs/docs/src/interfaces.ts
+++ libs/docs/src/interfaces.ts
@@ -1,193 +1,59 @@
-/**
- * the kind determines what properties are associated with given kind
- * 
- * - **primitive:** refers to built-in types, like `string` or `number`
- * - **reference:** refers to some established object, like `Date` or `Error`
- * - **type:** refers to a TypeScript-specific type described by `.type`
- */
-export type TypeKindRestricted = "primitive"|"reference"|"type"
-export interface GivenHasComment {
-	/**
-	 * Comment, if any, given on the type and usually formatted as Markdown
-	 */
-	comment?: string
-	/**
-	 * Flags are special comments with a name and a boolean value
-	 */
-	flags?: {
+import type { JSONSchema7 } from "json-schema"
+
+// NOTE: when adding a new "type", consider if it can be used as input on docs page
+/** Primitive types and the "object" type */
+export type BasicTypes = "string"|"number"|"bigint"|"boolean"|"symbol"|"undefined"|"null"|"Object"
+/** Object types that are commonly thought of as basic language constructs themselves */
+export type CommonTypes = "Function"|"Array"
+export interface PrimType {
+	/** Type as given in JavaScript, including common objects */
+	type: BasicTypes|CommonTypes
+	/** Typed data given from TypeScript formatted as JSON Schema */
+	typed?: JSONSchema7
+}
+
+export interface PrimComment {
+	comment: string
+}
+
+export interface PrimMethod {
+	name: string
+	path: string
+	signatures: PrimMethodSignature[]
+}
+
+export interface PrimParam extends PrimType, PrimComment {
+	name: string
+}
+export interface PrimReturn extends PrimType, PrimComment {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface PrimThrow extends PrimComment {}
+export interface PrimMethodSignature extends PrimComment {
+	params: PrimParam[]
+	returns: PrimReturn
+	throws: PrimThrow
+	flags: {
 		[flag: string]: boolean
 	}
 }
-/** Properties that usually deal with types */
-export interface TsTypeBase extends GivenHasComment {
-	/**
-	 * The name of the type. It could be:
-	 * 
-	 *   - A primitive type when `.kind` is "primitive"
-	 *   - A reference when `.kind` is "reference"
-	 *   - A defined type with a name
-	 */
+
+export interface PrimModule extends PrimComment {
 	name: string
-	/**
-	 * The kind of type given. This is used to determine what properties are given
-	 * on some given type. For instance, if `kind` is "type" then you may expect
-	 * to find a type.
-	 */
-	kind: TypeKindRestricted
-	/**
-	 * Given type can be omitted when `true`
-	 */
-	optional?: true
-}
-/** Variant of `TsTypeBase` specific to given `.kind` */
-export interface TsTypeSimple extends TsTypeBase {
-	kind: "object"|"primitive"
-}
-/** Variant of `TsTypeBase` specific to given `.kind` */
-export interface TsTypeInterface extends TsTypeBase {
-	kind: "interface"
-	/** Properties defined on the interface */
-	properties: {
-		[key: string|number|symbol]: TsType
-	}
-}
-/** Variant of `TsTypeBase` specific to given `.kind` */
-export interface TsTypeArray extends TsTypeBase {
-	kind: "array"
-	/** Determine if given type is intended to be used as a tuple or list */
-	tuple: boolean
-	items: TsType[]
-}
-/** Variant of `TsTypeBase` specific to given `.kind` */
-export interface TsTypeEnum extends TsTypeBase {
-	kind: "enum"
-	/** Members of enum given as key-value pairs */
-	members: {
-		[name: string]: string|number
-	}
-}
-/** Variant of `TsTypeBase` specific to given `.kind` */
-export interface TsTypeFunction extends TsTypeBase {
-	kind: "function"
-	signatures: PrimRpcSignature[]
-}
-/** A type for documentation that may be narrowed down to a specific kind */
-export type TsType = TsTypeSimple|TsTypeInterface|TsTypeArray|TsTypeEnum|TsTypeFunction
-
-/** Details concerning a throw directly from a given function */
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface PrimRpcThrow extends GivenHasComment {}
-/** Return value type for function with comment specific to function */
-export interface PrimRpcReturn extends GivenHasComment {
-	type: JSONSchema7
-}
-/** Parameter for function with comment specific to function  */
-export interface PrimRpcParam extends GivenHasComment {
-	name: string
-	type: JSONSchema7
-}
-/** A call signature for a function. Usually there is only one but a function could be overloaded */
-export interface PrimRpcSignature extends GivenHasComment {
-	/** Name of the function, same for all call signatures */
-	name: string // NOTE: name is already given in `PrimRpcStructure` key but provided here for easy access
-	/** Parameters expected by function */
-	params: PrimRpcParam[]
-	/** Return value for function */
-	returns: PrimRpcReturn
-	/** Throw condition, if any is given */
-	throws: PrimRpcThrow
-}
-
-export interface PrimRpcStructure extends GivenHasComment {
-	/** The name of the module-like structure. */
-	name: string // NOTE: name is already given in `PrimRpcStructure` key but provided here for easy access
-	/**
-	 * The module structure, including submodules, and all of its methods.
-	 * 
-	 * To navigate the structure, loop through object entries.
-	 * If given object value is an object, it is a submodule and if it is an array
-	 * then it is a list of call signatures for a function defined on the module.
-	 */
-	structure: {
-		/** The name of a module/module-like variable or a method name on that module */
-		[moduleOrMethodName: string]: PrimRpcStructure|PrimRpcSignature[]
+	path: string
+	flags: {
+		[flag: string]: boolean
 	}
 }
 
-/**
- * Functions used as RPC, organized by module and documented.
- */
-export interface PrimRpcDocs extends PrimRpcStructure {
-	/**
-	 * A list of method paths in the module as a one-dimensional list to easily
-	 * find method names in the given `.structure`. Path separator is "/".
-	 * 
-	 * This can be easily used with the "get" method provided in libraries like
-	 * [Lodash](https://lodash.com/docs#get) or
-	 * [Just](https://github.com/angus-c/just#just-safe-get).
-	 * 
-	 * Below is an an example of using a given method path to find details in `.structure`:
-	 * 
-	 * ```ts
-	 * // methodPath == "submodule/sayHello"
-	 * const path = methodPath.split("/").map(path => ["structure", path]).flat()
-	 * // path == ["structure", "submodule", "structure", "sayHello"]
-	 * const foundDocumentation = get(docs, path) // `docs` is generated RPC docs
-	 * ```
-	 */
-	methods: string[]
-	/**
-	 * A list of module paths in the module as a one-dimensional list to easily
-	 * find module names in the given `.structure`. Path separator is "/".
-	 * 
-	 * This can be easily used with the "get" method provided in libraries like
-	 * [Lodash](https://lodash.com/docs#get) or
-	 * [Just](https://github.com/angus-c/just#just-safe-get).
-	 * 	 * Below is an an example of using a given method path to find details in `.structure`:
-	 * 
-	 * ```ts
-	 * // modulePath == "submodule/sayHello"
-	 * const path = modulePath.split("/").map(path => ["structure", path]).flat()
-	 * // path == ["structure", "submodule", "structure", "sayHello"]
-	 * const foundDocumentation = get(docs, path) // `docs` is generated RPC docs
-	 * ```
-	 */
-	modules: string[]
+export type PrimRootStructureKeys = keyof Omit<PrimRpcDocs, "props"|"docs">
+export interface PrimModuleStructure {
+	docs: [key: PrimRootStructureKeys, index: number]
+	props?: {
+		[moduleOrMerhodName: string]: PrimModuleStructure
+	}
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const example = {
-	name: "@doseofted/prim-example",
-	comment: "Test ",
-	methods: {
-		"sayHello": [
-			{
-				name: "sayHello",
-				params: [{
-					name: "greeting",
-					type: {
-						type: "string",
-					},
-				}, {
-					name: "name",
-					type: {
-						type: "string",
-					},
-				}],
-				returns: {
-					comment: "A nice greeting",
-					type: {
-						type: "string",
-					},
-				},
-				throws: {
-					comment: "",
-				},
-			},
-		],
-	},
-	modules: [],
-	structure: {
-		sayHello: true,
-	},
+export interface PrimRpcDocs extends PrimModuleStructure {
+	methods: PrimMethod[]
+	modules: PrimModule[]
 }
