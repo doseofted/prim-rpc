e3b2162d217147c5bc00cb80b437090b92cc8b86
Ted Klingenberg <ted@doseofted.com>
2022-10-17 23:23:36 -0400

    Canvas element can now track position of HTML elements placed within context for background effects

--- libs/ui/src/components/Lights.tsx
+++ libs/ui/src/components/Lights.tsx
@@ -1,31 +1,91 @@
-import { Component, onCleanup, onMount, JSX, splitProps, createContext, useContext, createEffect, createSignal } from "solid-js"
-import { CanvasSpace } from "pts"
+import { Component, onCleanup, onMount, JSX, splitProps, createContext, useContext } from "solid-js"
+import { createStore, produce, SetStoreFunction } from "solid-js/store"
+import { CanvasSpace, Pt } from "pts"
+
+interface LightOptions {
+	brightness: number
+	color: string
+}
+
+interface LightInstance extends LightOptions {
+	id: number
+	x: number
+	y: number
+}
+
+const LightsContext = createContext<[get: LightInstance[], set: SetStoreFunction<LightInstance[]>]>()
+
+export function LightsProvider(props: { children: JSX.Element | JSX.Element[] }) {
+	// eslint-disable-next-line solid/reactivity, @typescript-eslint/no-unused-vars
+	const lights = createStore<LightInstance[]>([])
+	return (
+		<LightsContext.Provider value={lights}>
+			<Lights style={{ position: "absolute", width: "100%", height: "100%", top: 0, left: 0 }} />
+			{props.children}
+		</LightsContext.Provider>
+	)
+}
+export function useLights() {
+	return useContext(LightsContext)
+}
+
+export const Light: Component = () => {
+	const [, setLights] = useLights() ?? []
+	let div: HTMLDivElement
+	let observer: ResizeObserver
+	onMount(() => {
+		const { x, y } = div.getBoundingClientRect()
+		function setupResizeObserver(givenIndex: number) {
+			observer = new ResizeObserver((entries) => {
+				for (const entry of entries) {
+					const { target } = entry
+					const { x, y } = target.getBoundingClientRect()
+					setLights?.(produce(state => {
+						state[givenIndex].x = x
+						state[givenIndex].y = y
+					}))
+				}
+			})
+			observer.observe(div)
+			onCleanup(() => observer.disconnect())
+		}
+		setLights?.(produce(state => {
+			const givenIndex = state.push({
+				id: state.length + 1,
+				x, y,
+				brightness: 1,
+				color: "#fff",
+			}) - 1
+			setupResizeObserver(givenIndex)
+		}))
+	})
+	return <div ref={e => div = e} />
+}
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props extends JSX.HTMLAttributes<HTMLDivElement> {
 	// ...
 }
-
-// eslint-disable-next-line solid/reactivity
-export const LightsContext = createContext(createSignal(1))
-
 const Lights: Component<Props> = (p) => {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [_props, attrs] = splitProps(p, [])
-	const [lightsCtx] = useContext(LightsContext)
+	const [lights = []] = useLights() ?? []
 	let canvas: HTMLCanvasElement | undefined
 	let space: CanvasSpace | undefined
-	createEffect(() => {
-		console.log("Lights context:", lightsCtx())
-	})
+	/* createEffect(() => {
+		console.log(lights?.map(({ x, y }) => `${x}x${y}`).join(", "))
+	}) */
 	onMount(() => {
 		if (!canvas) { return }
 		space = new CanvasSpace(canvas)
-		space.setup({ bgcolor: "#fff", resize: true })
+		space.setup({ bgcolor: "#000", resize: true })
 		const form = space.getForm()
 		space.add(() => {
 			if (!space) { return }
-			form.point(space.pointer, lightsCtx())
+			for (const light of lights) {
+				const { x, y } = light
+				form.point(new Pt({ x, y }), 10)
+			}
 		})
 		space.play().bindMouse()
 	})
--- libs/ui/src/pages/index.tsx
+++ libs/ui/src/pages/index.tsx
@@ -2,22 +2,33 @@ import { Component, createSignal } from "solid-js"
 import Docs from "../components/Docs"
 import docs from "@doseofted/prim-example/dist/docs.json"
 import { addFolderToPane, addSignalInput, addSignalMonitor } from "../utils/tweakpane"
-import Lights, { LightsContext } from "../components/Lights"
+import { Light, LightsProvider } from "../components/Lights"
 
 const Index: Component = () => {
 	const folder = addFolderToPane({ title: "A Folder" })
 	// eslint-disable-next-line solid/reactivity
 	const numSignal = addSignalInput(createSignal(0), "number", { min: 0, max: 100, step: 1 }, folder)
 	const [num, setNum] = numSignal
+	const size = () => `${num()}px`
 	addSignalMonitor(numSignal, "number", { view: "graph", min: 0, max: 100 }, folder)
+	const TestOnly = () => (
+		<div style={{ width: size(), height: size() }} class="bg-gray border-white m-6 border-2">
+			<Light />
+		</div>
+	)
 	return (
-		<LightsContext.Provider value={numSignal}>
-			<Lights style={{ position: "absolute", width: "100%", height: "100vh", top: 0, left: 0 }} />
+		<LightsProvider>
+			{/* <Lights style={{ position: "absolute", width: "100%", height: "100vh", top: 0, left: 0 }} /> */}
 			<div onClick={() => setNum(num() + 1)}>
 				This is a page. That's a number: {num()}
 			</div>
+			<div class="lights-and-stuff flex relative">
+				<TestOnly />
+				<TestOnly />
+
+			</div>
 			<Docs docs={docs} />
-		</LightsContext.Provider>
+		</LightsProvider>
 	)
 }
 
