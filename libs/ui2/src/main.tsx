a0b1c9239c0db95cfa529f0d9401df9537263292
Ted Klingenberg <ted@doseofted.com>
2022-11-12 20:08:01 -0500

    Added new dependencies to "ui2" project, still working

--- libs/ui2/src/main.tsx
+++ libs/ui2/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+/* @refresh reload */
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter as Router, useRoutes } from "react-router-dom"
+import routes from "~react-pages"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const App = () => {
+  return <React.Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</React.Suspense>
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 )
---
+++ libs/ui2/src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Index = () => {
+	return <div>Hello</div>
+}
+
+export default Index
