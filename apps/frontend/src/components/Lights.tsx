dbc1ce80e21df9e988a70af1a7161945d186afe1
Ted Klingenberg <ted@doseofted.com>
2022-11-20 08:52:01 -0500

    Added more options

--- apps/frontend/src/components/Lights.tsx
+++ apps/frontend/src/components/Lights.tsx
@@ -2,6 +2,7 @@ import { createContext, useContext, useEffect, useMemo, useRef, useState } from 
 import { useImmer } from "use-immer"
 import { nanoid } from "nanoid"
 import { useWindowScroll, useWindowSize } from "react-use"
+import { random, shuffle } from "lodash-es"
 
 export interface LightOptions {
 	/** Brightness from 0-2 */
@@ -31,6 +32,7 @@ type LightsState = {
 	}
 	windowSize: { width: number; height: number }
 	windowScroll: { x: number; y: number }
+	colors: string[]
 }
 type LightsActions = {
 	createLight(opts: LightOptions, position: [x: number, y: number]): string
@@ -49,6 +51,9 @@ function useLights() {
 	return ctx
 }
 
+const defaultColors = ["#f0A3FF", "#6D53FF", "#1D0049", "#0069BA", "#5BB8FF"]
+const defaultBackground = "#2D0D60"
+
 export interface LightsProps {
 	children?: React.ReactNode | React.ReactNode[]
 	/** Options used if none are provided to a light, overrides defaults */
@@ -62,14 +67,16 @@ export interface LightsProps {
 	/** Event handler on the first frame of animation */
 	onFirstFrame?: () => void
 }
+/** Provider for `Light` components */
 export function Lights(props: LightsProps) {
-	const { children, options: optionsShared } = props
+	const { children, colors: colorsGiven, options: optionsShared } = props
 	const [lights, setLights] = useImmer<{ [id: string]: LightInstance }>({})
 	const windowSize = useWindowSize(0, 0)
 	const windowScroll = useWindowScroll()
+	const colors = useMemo(() => colorsGiven ?? defaultColors, [colorsGiven])
 	const ctx = useMemo<LightsContext>(
 		() => [
-			{ lights, windowSize, windowScroll },
+			{ lights, windowSize, windowScroll, colors },
 			{
 				createLight(opts, position) {
 					const id = nanoid()
@@ -125,12 +132,34 @@ export interface LightProps extends React.HTMLAttributes<HTMLDivElement> {
 	children?: React.ReactNode | React.ReactNode[]
 	options?: Partial<LightOptions>
 }
+/**
+ * Place a `Light` component anywhere inside of a `Lights` component and a light
+ * will glow behind the given element. `Light` components are just a "div"
+ * element which can be used to either encapsulate some other element or be
+ * used inside of some other container.
+ *
+ * @example
+ *
+ * ```tsx
+ * <Lights>
+ *   <div class="flex">
+ *     <Light>
+ *       <p>It glows!</p>
+ *     </Light>
+ *     <Light>
+ *       <p>This glows too!</p>
+ *     </Light>
+ *   </div>
+ * </Lights>
+ * ```
+ */
 export function Light(props: LightsProps) {
 	const { options: givenOptions, children, ...attrs } = props
 	const [ctx, actions] = useLights()
+	const color = useMemo(() => shuffle(ctx.colors)[random(0, ctx.colors.length - 1)], [])
 	const options = useMemo<LightOptions>(
 		() => ({
-			color: "#000",
+			color,
 			size: 50,
 			offset: [0, 0],
 			delay: 50,
@@ -157,6 +186,9 @@ export function Light(props: LightsProps) {
 		const { x, y } = getCenter(div.current)
 		actions.updateLightPosition(id, [x, y])
 	}, [ctx.windowSize, ctx.windowScroll])
+	useEffect(() => {
+		actions.updateLightOptions(id, options)
+	}, [options])
 	const div = useRef<HTMLDivElement>(null)
 	return (
 		<div {...attrs} ref={div}>
