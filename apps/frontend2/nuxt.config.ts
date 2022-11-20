0193884d8103c3d848d20c6aac24bf5253266fdf
Ted Klingenberg <ted@doseofted.com>
2022-11-19 19:28:01 -0500

    Set up a Nuxt 3 project as alternative to Next.js, WIP (simpler to work with, now works with prim-ui/vue export, will allow me to more easiy build a Nuxt module)

---
+++ apps/frontend2/nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+export default defineNuxtConfig({
+	srcDir: "src",
+})
---
+++ apps/frontend2/src/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <PrimTest name="Ted" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PrimTest } from "@doseofted/prim-ui/vue"
+</script>
