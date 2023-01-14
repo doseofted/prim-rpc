33e51577ea9fcb8024819896a9d2e4d05075c2d1
Ted Klingenberg <ted@doseofted.com>
2023-01-14 18:18:10 -0500

    Added (possible) CLI for generating RPC-specific docs without having to write it into a script, not used yet

--- libs/tooling/build.config.js
+++ libs/tooling/build.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup"
 
 export default defineConfig(options => ({
 	...options,
-	entry: ["src/docs/index.ts", "src/build/index.ts"],
+	entry: ["src/docs/index.ts", "src/build/index.ts", "src/cli/index.ts"],
 	target: "ES2020",
 	dts: true,
 	sourcemap: true,
---
+++ libs/tooling/src/cli/index.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { echo } from "zx"
+
+echo`Prim+RPC CLI isn't ready just yet. Check back soon!`
