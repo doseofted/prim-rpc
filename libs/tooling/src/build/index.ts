ae34f9546d761e89fa5c437cd24cd903e80d69b0
Ted Klingenberg <ted@doseofted.com>
2023-01-14 18:03:20 -0500

    Removed usage of "__dirname" in tooling/build (I think Vite provides this as fallback for config but shouldn't be assumed for other bundlers)

--- libs/tooling/src/build/index.ts
+++ libs/tooling/src/build/index.ts
@@ -3,10 +3,16 @@ import { createUnplugin } from "unplugin"
 interface BuildOptions {
 	/** Folder name, relative to project folder that should not be imported */
 	name: string
+	/** Enable console logs for imports to determine what name option should be used */
+	debug?: boolean
 }
 
 export default createUnplugin((options: BuildOptions = { name: "" }) => ({
-	name: "prim-prevent-import",
-	transformInclude: id => id.startsWith(__dirname + options.name),
+	name: "unplugin-prim-prevent-import",
+	transformInclude: id => {
+		const matches = id.startsWith(options.name)
+		if (options.debug) console.log((matches ? "matc:" : "no match:").padEnd(10), id)
+		return matches
+	},
 	transform: () => "export {};",
 }))
