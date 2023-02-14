4b71cfa7d08d7bbba93bab8eabdcb72d964e19f9
Ted Klingenberg <ted@doseofted.com>
2023-02-13 22:53:04 -0500

    Added h3 method handler (not tested yet)

--- libs/plugins/build.config.js
+++ libs/plugins/build.config.js
@@ -7,6 +7,7 @@ export default defineConfig(options => ({
 		// TODO: entries need to separated into their own packages since packageJson.exports won't be a useful setting any time soon
 		"src/server/fastify.ts",
 		"src/server/express.ts",
+		"src/server/h3.ts",
 		"src/server/ws.ts",
 		"src/client/axios.ts",
 		"src/client/browser.ts",
--- libs/plugins/src/server/h3.ts
+++ libs/plugins/src/server/h3.ts
@@ -1,2 +1,70 @@
-// TODO: write this plugin (for Nuxt 3 / Nitro / h3)
-export {}
+import type { PrimServerMethodHandler, PrimServerEvents } from "@doseofted/prim-rpc"
+import {
+	defineEventHandler,
+	readRawBody,
+	getMethod,
+	setResponseHeaders,
+	getHeader,
+	setResponseStatus,
+	readMultipartFormData,
+	App,
+	H3Event,
+} from "h3"
+import { mkdtemp, writeFile } from "node:fs/promises"
+import { tmpdir } from "node:os"
+import { join as joinPath } from "node:path"
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface SharedH3Options {
+	contextTransform?: (event: H3Event) => unknown
+}
+
+interface PrimH3PluginOptions extends SharedH3Options {
+	prim: PrimServerEvents
+}
+
+export function defineH3PrimHandler(options: PrimH3PluginOptions) {
+	const { prim, contextTransform } = options
+	let body: string
+	const blobs: { [identifier: string]: unknown } = {}
+	return defineEventHandler(async event => {
+		const givenPath = new URL(event.node.req.url).pathname
+		if (!givenPath.startsWith(prim.options.prefix)) {
+			return
+		}
+		const requestType = getHeader(event, "content-type")
+		const method = getMethod(event)
+		const url = event.node.req.url
+		const context = contextTransform(event)
+		if (requestType.startsWith("multipart/form-data")) {
+			const parts = await readMultipartFormData(event)
+			for (const part of parts) {
+				if (part.name === "rpc") {
+					body = part.data.toString("utf-8")
+				} else if (part.filename && part.name.startsWith("_bin_")) {
+					const tmpFolder = await mkdtemp(joinPath(tmpdir(), "prim-rpc-"))
+					const tmpFile = joinPath(tmpFolder, part.filename)
+					await writeFile(tmpFile, part.data)
+					blobs[part.name] = tmpFile
+				}
+			}
+		} else {
+			body = await readRawBody(event)
+		}
+		const result = await prim.server().call({ body, method, url }, blobs, context)
+		setResponseStatus(event, result.status)
+		setResponseHeaders(event, result.headers)
+		return result.body
+	})
+}
+
+interface MethodH3Options extends SharedH3Options {
+	app: App
+}
+
+export const createMethodHandler = (options: MethodH3Options): PrimServerMethodHandler => {
+	const { app } = options
+	return prim => {
+		app.use(defineH3PrimHandler({ ...options, prim }))
+	}
+}
