99a29f080155b77d47ca160a36e84c9f2d4f358f
Ted Klingenberg <ted@doseofted.com>
2023-02-16 11:59:45 -0500

    Added reference method handler to use for creation of new method handlers (example resembles a method handler for an Express-like framework)

---
+++ libs/plugins/src/server/template/method.ts
@@ -0,0 +1,55 @@
+import type { PrimServerMethodHandler, PrimServerEvents } from "@doseofted/prim-rpc"
+
+/** NOTE: replace `__SERVER__` with intended server name. note that `Example` is simply that: an example interface. */
+type __SERVER__ = {
+	use: (middleware: (req: Example["req"], res: Example["res"], next: Example["next"]) => Promise<void> | void) => void
+}
+interface Example {
+	req: {
+		method: string
+		url: string
+		body: string
+	}
+	res: {
+		status: (code: number) => Example["res"]
+		setHeaders: (headers: Record<string, string>) => Example["res"]
+		send: (message: string) => Example["res"]
+	}
+	next: () => void
+}
+
+export type Prim__SERVER__Context = { context: "__SERVER__"; req: Example["req"]; res: Example["res"] }
+
+interface Shared__SERVER__Options {
+	contextTransform?: (req: Example["req"], res: Example["res"]) => Prim__SERVER__Context
+}
+
+interface Prim__SERVER__Options extends Shared__SERVER__Options {
+	prim: PrimServerEvents
+}
+
+export function define__SERVER__Handler(options: Prim__SERVER__Options) {
+	const { prim, contextTransform = (req, res) => ({ context: "__SERVER__", req, res }) } = options
+	// NOTE: this is some sort of event handler that your chosen server framework expects (like a middleware/plugin)
+	return async (req: Example["req"], res: Example["res"], next: Example["next"]) => {
+		if (!new URL(req.url).pathname.startsWith(prim.options.prefix)) {
+			return next()
+		}
+		// NOTE: this example demonstrates a simple request without files (multipart data should be considered in real plugin)
+		const { method, url, body } = req
+		const context = contextTransform(req, res)
+		const { body: response, headers, status } = await prim.server().call({ method, url, body }, {}, context)
+		res.setHeaders(headers).status(status).send(response)
+	}
+}
+
+interface MethodH3Options extends Shared__SERVER__Options {
+	app: __SERVER__
+}
+
+export const createMethodHandler = (options: MethodH3Options): PrimServerMethodHandler => {
+	const { app } = options
+	return prim => {
+		app.use(define__SERVER__Handler({ ...options, prim }))
+	}
+}
