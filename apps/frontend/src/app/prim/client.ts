bd3969a42f8539bd9b00019097d031a21a7172b4
Ted Klingenberg <ted@doseofted.com>
2023-06-11 17:51:21 -0400

    Using deployed URL on server and local path on client

--- apps/frontend/src/app/prim/client.ts
+++ apps/frontend/src/app/prim/client.ts
@@ -6,8 +6,12 @@ import type * as module from "@doseofted/prim-example"
 // const host = process.env.NEXT_PUBLIC_WEBSITE_HOST ?? ""
 // const contained = JSON.parse(process.env.NEXT_PUBLIC_CONTAINED ?? "false") as boolean
 
-console.log(process.env)
-const endpoint = typeof window === "undefined" ? "http://localhost:3000/prim" : "/prim"
+const endpoint =
+	typeof window === "undefined"
+		? process.env.VERCEL_URL
+			? `https://${process.env.VERCEL_URL}/prim`
+			: "http://localhost:3000/prim"
+		: "/prim"
 const client = createPrimClient<typeof module>({
 	endpoint,
 	jsonHandler,
