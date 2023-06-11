3570a6918333f6cac71c9e2107d75972883c7cd3
Ted Klingenberg <ted@doseofted.com>
2023-06-11 17:24:02 -0400

    Adjusted modules allowed in production, simplified client setup in docs

--- apps/frontend/src/app/prim/[[...prim]]/route.ts
+++ apps/frontend/src/app/prim/[[...prim]]/route.ts
@@ -3,7 +3,7 @@ import { defineNextjsAppHandler } from "@doseofted/prim-rpc-plugins/nextjs"
 import * as moduleAll from "@doseofted/prim-example"
 import jsonHandler from "superjson"
 
-const module = process.env.NODE_ENV === "development" ? moduleAll : { sayHello: moduleAll.sayHello }
+const module = process.env.NODE_ENV === "development" ? moduleAll : { greetings: moduleAll.greetings }
 const prim = createPrimServer({ module, jsonHandler })
 export const { GET, POST } = defineNextjsAppHandler({
 	prim,
--- apps/frontend/src/app/prim/client.ts
+++ apps/frontend/src/app/prim/client.ts
@@ -3,15 +3,10 @@ import { createMethodPlugin, createCallbackPlugin } from "@doseofted/prim-rpc-pl
 import jsonHandler from "superjson"
 import type * as module from "@doseofted/prim-example"
 
-const host = process.env.NEXT_PUBLIC_WEBSITE_HOST ?? ""
-const serverSide = typeof window === "undefined"
-const contained = JSON.parse(process.env.NEXT_PUBLIC_CONTAINED ?? "false") as boolean
+// const host = process.env.NEXT_PUBLIC_WEBSITE_HOST ?? ""
+// const contained = JSON.parse(process.env.NEXT_PUBLIC_CONTAINED ?? "false") as boolean
 
-// https://github.com/vercel/next.js/issues/44062#issuecomment-1445183293
-const serverUrl = "http://127.0.0.1:3000/prim"
-const clientUrl = contained ? `https://api.${host}/prim` : serverUrl
-const endpoint = serverSide ? serverUrl : clientUrl
-
+const endpoint = typeof window === "undefined" ? "http://localhost:3000/prim" : "/prim"
 const client = createPrimClient<typeof module>({
 	endpoint,
 	jsonHandler,
