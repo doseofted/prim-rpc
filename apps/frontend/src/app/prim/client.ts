778fa9e20e83be806770f54cfadf1a84289ef3c6
Ted Klingenberg <ted@doseofted.com>
2023-06-21 15:41:57 -0400

    Documentation site now uses dynamic import for example function call on homepage

--- apps/frontend/src/app/prim/client.ts
+++ apps/frontend/src/app/prim/client.ts
@@ -1,19 +1,13 @@
 import { createPrimClient } from "@doseofted/prim-rpc"
 import { createMethodPlugin, createCallbackPlugin } from "@doseofted/prim-rpc-plugins/browser"
 import jsonHandler from "superjson"
-import type * as module from "@doseofted/prim-example"
+import type * as moduleType from "@doseofted/prim-example"
 
 // const host = process.env.NEXT_PUBLIC_WEBSITE_HOST ?? ""
 // const contained = JSON.parse(process.env.NEXT_PUBLIC_CONTAINED ?? "false") as boolean
 
-const endpoint =
-	typeof window === "undefined"
-		? process.env.VERCEL_URL
-			? `https://${process.env.VERCEL_URL}/prim`
-			: "http://localhost:3000/prim"
-		: "/prim"
-const client = createPrimClient<typeof module>({
-	endpoint,
+const client = createPrimClient<typeof moduleType>({
+	module: typeof window === "undefined" ? import("@doseofted/prim-example") : null,
 	jsonHandler,
 	methodPlugin: createMethodPlugin(),
 	callbackPlugin: createCallbackPlugin(),
