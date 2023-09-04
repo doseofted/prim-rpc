0d3fa8f99bef02e139ec1e354e695239884277bd
Ted Klingenberg <ted@doseofted.com>
2023-09-04 16:32:53 -0400

    Added code banner to build output

--- libs/plugins/build.config.js
+++ libs/plugins/build.config.js
@@ -1,6 +1,11 @@
 // @ts-check
 import { defineConfig } from "tsup"
 
+const banner = `
+// Part of the Prim+RPC project ( https://prim.doseofted.me/ )
+// Copyright 2023 Ted Klingenberg
+// SPDX-License-Identifier: Apache-2.0`.replace(/^\n/g, "")
+
 export default defineConfig({
 	entry: [
 		// TODO: entries need to separated into their own packages in the future so they can be versioned separately
@@ -21,4 +26,5 @@ export default defineConfig({
 	format: ["esm"],
 	dts: true,
 	clean: true,
+	banner: { js: banner },
 })
--- libs/rpc/build.config.js
+++ libs/rpc/build.config.js
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from "tsup"
 
+const banner = `
+// Part of the Prim+RPC project ( https://prim.doseofted.me/ )
+// Copyright 2023 Ted Klingenberg
+// SPDX-License-Identifier: Apache-2.0`.replace(/^\n/g, "")
+
 export default defineConfig({
 	entry: ["src/index.ts"],
 	target: "es2020",
 	format: ["esm"],
 	dts: true,
 	clean: true,
+	banner: { js: banner },
 })
