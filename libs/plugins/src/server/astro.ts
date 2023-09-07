3fbc99c05da5fe8635bf3eaae23231e0a36716a0
Ted Klingenberg <ted@doseofted.com>
2023-09-07 19:20:04 -0400

    Astro handler now supports files in RPC result

--- libs/plugins/src/server/astro.ts
+++ libs/plugins/src/server/astro.ts
@@ -4,22 +4,18 @@
 
 import type { BlobRecords, PrimServerEvents } from "@doseofted/prim-rpc"
 import type { APIRoute, APIContext, Props } from "astro"
-import { type FileForEnvType, useFileForEnv } from "../utils/isomorphic"
 
 interface PrimAstroPluginOptions {
 	prim: PrimServerEvents
-	headers?: Headers | Record<string, string>
 	contextTransform?: (request: APIContext<Props>) => { context: "astro"; request: Request }
 }
 
-let FileForEnv: FileForEnvType
-
 export function defineAstroPrimHandler(options: PrimAstroPluginOptions) {
-	const { prim, headers = {}, contextTransform = request => ({ context: "astro", request }) } = options
+	const { prim, contextTransform = request => ({ context: "astro", request }) } = options
+	const { jsonHandler } = prim.options
 	const handler: APIRoute = async ctx => {
-		FileForEnv ??= await useFileForEnv()
 		const { request, url: urlFull } = ctx
-		let body: string
+		let body: string | ArrayBuffer
 		const blobs: BlobRecords = {}
 		const url = urlFull.pathname + urlFull.search
 		if (!url.startsWith(prim.options.prefix)) {
@@ -29,20 +25,51 @@ export function defineAstroPrimHandler(options: PrimAstroPluginOptions) {
 		const method = request.method
 		if (requestType.startsWith("multipart/form-data")) {
 			const formData = await request.formData()
-			formData.forEach((value, key) => {
+			for (const [key, value] of formData) {
 				if (key === "rpc") {
-					body = value.toString()
-				} else if (key.startsWith("_bin_") && value instanceof FileForEnv) {
+					body = value instanceof Blob && jsonHandler.binary ? await value.arrayBuffer() : value.toString()
+				} else if (key.startsWith("_bin_") && value instanceof Blob) {
 					blobs[key] = value
 				}
-			})
+			}
 		} else if (method === "POST") {
 			body = await request.text()
 		}
 		const result = await prim.server().call({ body, method, url, blobs }, contextTransform(ctx))
+		const hasBinary = ["application/octet-stream", "multipart/form-data"].includes(result.headers["content-type"])
+		let firstFile = { name: "", blob: null as Blob | null, type: "application/octet-stream" }
+		const blobEntries = Object.entries(result.blobs)
+		if (hasBinary) {
+			const formData = new FormData()
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+			formData.append("rpc", result.body)
+			for (const [key, value] of blobEntries) {
+				formData.append(key, value as Blob)
+				if (!firstFile.blob) {
+					const blob = value as Blob
+					firstFile = { name: blob.name, blob, type: blob.type }
+				}
+			}
+			if (method === "POST" && blobEntries.length > 0) {
+				delete result.headers["content-type"] // NOTE: Response will handle this
+				return new Response(formData, {
+					headers: result.headers,
+					status: result.status,
+				})
+			} else if (method === "GET" && blobEntries.length === 1) {
+				return new Response(firstFile.blob, {
+					headers: {
+						...result.headers,
+						"content-disposition": `inline; filename="${firstFile.name}"`,
+						"content-type": firstFile.type,
+					},
+					status: result.status,
+				})
+			}
+		}
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 		return new Response(result.body, {
-			headers: { ...result.headers, ...headers },
+			headers: result.headers,
 			status: result.status,
 		})
 	}
