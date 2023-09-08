2aad319ed4a9ca2e0c07fe8f8f3076b47784fe57
Ted Klingenberg <ted@doseofted.com>
2023-09-07 20:02:42 -0400

    Next.js handler now supports files in RPC result

--- libs/plugins/src/server/nextjs.ts
+++ libs/plugins/src/server/nextjs.ts
@@ -10,14 +10,14 @@ interface SharedNextjsOptions {
 }
 
 interface PrimNextjsAppPluginOptions extends SharedNextjsOptions {
-	headers?: Headers | Record<string, string>
 	contextTransform?: (request: Request) => { context: "nextjs-app"; request: Request }
 }
 
 export function defineNextjsAppPrimHandler(options: PrimNextjsAppPluginOptions) {
-	const { prim, headers = {}, contextTransform = request => ({ context: "nextjs-app", request }) } = options
+	const { prim, contextTransform = request => ({ context: "nextjs-app", request }) } = options
+	const { jsonHandler } = prim.options
 	async function handler(request: Request) {
-		let body: string
+		let body: string | ArrayBuffer
 		const blobs: BlobRecords = {}
 		const parts = new URL(request.url)
 		const url = parts.pathname + parts.search
@@ -28,22 +28,52 @@ export function defineNextjsAppPrimHandler(options: PrimNextjsAppPluginOptions) 
 		const method = request.method
 		const context = contextTransform(request)
 		if (requestType.startsWith("multipart/form-data")) {
-			const FileObj = typeof File === "undefined" ? (await import("node:buffer")).File : File
 			const formData = await request.formData()
-			formData.forEach((value, key) => {
+			for (const [key, value] of formData) {
 				if (key === "rpc") {
-					body = value.toString()
-				} else if (key.startsWith("_bin_") && value instanceof FileObj) {
+					body = value instanceof Blob && jsonHandler.binary ? await value.arrayBuffer() : value.toString()
+				} else if (key.startsWith("_bin_") && value instanceof Blob) {
 					blobs[key] = value
 				}
-			})
+			}
 		} else if (method === "POST") {
 			body = await request.text()
 		}
 		const result = await prim.server().call({ body, method, url, blobs }, context)
+		const hasBinary = ["application/octet-stream", "multipart/form-data"].includes(result.headers["content-type"])
+		let firstFile = { name: "", blob: null as Blob | null, type: "application/octet-stream" }
+		const blobEntries = Object.entries(result.blobs)
+		if (hasBinary) {
+			const formData = new FormData()
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+			formData.append("rpc", result.body)
+			for (const [key, value] of blobEntries) {
+				formData.append(key, value as Blob)
+				if (!firstFile.blob) {
+					const blob = value as Blob
+					firstFile = { name: blob.name, blob, type: blob.type }
+				}
+			}
+			if (method === "POST" && blobEntries.length > 0) {
+				delete result.headers["content-type"] // NOTE: Response will handle this
+				return new Response(formData, {
+					headers: result.headers,
+					status: result.status,
+				})
+			} else if (method === "GET" && blobEntries.length === 1) {
+				return new Response(firstFile.blob, {
+					headers: {
+						...result.headers,
+						"content-disposition": `inline; filename="${firstFile.name}"`,
+						"content-type": firstFile.type,
+					},
+					status: result.status,
+				})
+			}
+		}
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 		return new Response(result.body, {
-			headers: { ...result.headers, ...headers },
+			headers: result.headers,
 			status: result.status,
 		})
 	}
