fa85fc7889c1a41e79878e8b67a1a8ce2c69b064
Ted Klingenberg <ted@doseofted.com>
2023-09-25 15:35:28 -0400

    Created working version of tabs that is useable from Astro and markdown

--- apps/documentation/src/components/Code/TabsTest.react.tsx
+++ apps/documentation/src/components/Code/TabsTest.react.tsx
@@ -2,27 +2,34 @@ import { Tabs } from "@ark-ui/react"
 
 // Slot key names need to be named for usage in Astro
 type TabsTestProps = {
-	[key in `tab${0 | 1 | 2 | 3 | 4 | 5}`]: React.ReactHTMLElement<HTMLElement>
+	[key in `$tab${0 | 1 | 2 | 3 | 4 | 5}`]: React.ReactHTMLElement<HTMLElement>
+}
+type Props = TabsTestProps & {
+	tabs?: { name: string; icon: string }[]
 }
 
-export function TabsTest(props: TabsTestProps) {
+export function TabsTest(props: Props) {
+	const { tabs: detailsTabs } = props
 	const tabs: React.ReactHTMLElement<HTMLElement>[] = []
 	for (const [key, value] of Object.entries(props)) {
-		if (!key.startsWith("tab")) continue
-		tabs.push(value)
+		if (!key.startsWith("$tab")) continue
+		tabs.push(value as React.ReactHTMLElement<HTMLElement>)
 	}
 	return (
-		<Tabs.Root defaultValue="react">
-			<Tabs.List>
-				<Tabs.Trigger value="react">React</Tabs.Trigger>
-				<Tabs.Trigger value="vue">Vue</Tabs.Trigger>
-				<Tabs.Trigger value="solid">Solid</Tabs.Trigger>
-				<Tabs.Indicator />
+		<Tabs.Root defaultValue={detailsTabs?.[0].name}>
+			<Tabs.List className="tabs">
+				{detailsTabs?.map(({ name, icon }, index) => (
+					<Tabs.Trigger key={index} className={["tab tab-bordered"].join(" ")} value={name}>
+						{name}
+					</Tabs.Trigger>
+				))}
+				<Tabs.Indicator className="tab tab-bordered tab-active" />
 			</Tabs.List>
-			{tabs.map(tab => {
-				console.log(tab.props.value)
-				return <Tabs.Content value="react">{tab}</Tabs.Content>
-			})}
+			{tabs.map((tab, index) => (
+				<Tabs.Content key={index} value={detailsTabs?.[index].name ?? ""}>
+					{tab}
+				</Tabs.Content>
+			))}
 		</Tabs.Root>
 	)
 }
