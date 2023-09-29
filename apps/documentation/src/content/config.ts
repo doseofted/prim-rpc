61184dea692b823fbc0ae057e2f987103031bd8c
Ted Klingenberg <ted@doseofted.com>
2023-09-29 09:05:27 -0400

    Table of Contents is now created manually, easier to understand than auto-generated version

--- apps/documentation/src/content/config.ts
+++ apps/documentation/src/content/config.ts
@@ -1,32 +1,71 @@
-import { defineCollection, z } from "astro:content"
+import { defineCollection, type getCollection, z } from "astro:content"
 
-const documentationCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		order: z.number().optional(),
-		description: z.string().optional(),
-	}),
-})
+export interface TableOfContentsSection {
+	name: string
+	link?: string
+	sections?: TableOfContentsSection[]
+}
+export type TableOfContents = TableOfContentsSection[]
 
-const pluginsCollection = defineCollection({
-	type: "content",
-	schema: z.object({
-		title: z.string(),
-		order: z.number().optional(),
-		icon: z.string().optional(),
-		type: z.enum(["method-handler", "method-plugin", "callback-handler", "callback-plugin", "json-handler"]),
-		transport: z.enum(["http", "ws"]),
-		features: z.array(z.string()).optional(),
-	}),
-})
+export const versionsAvailable = [{
+	name: "v0",
+	aliases: ["latest"],
+	available: true,
+}]
+export const latestVersion = "v0"
+export const documentationCollections = ["docs", "plugins"] as const
 
-const apiCollection = defineCollection({
-	type: "data",
-})
+/** Table of Contents for Documentation pages */
+export const documentationContents = (collection = "docs", version: `v${string}` = latestVersion): TableOfContents => [
+	{
+		version: "v0",
+		content: [
+			{
+				name: "Library Usage",
+				sections: [
+					{ name: "Introduction", link: `/${collection}/${version}/usage/introduction` },
+					{ name: "Setup", link: `/${collection}/${version}/usage/setup` },
+					{ name: "How to Use", link: `/${collection}/${version}/usage/how-to-use` },
+					{ name: "Examples", link: `/${collection}/${version}/usage/examples` },
+					{ name: "Security", link: `/${collection}/${version}/usage/security` },
+					{ name: "Limitations", link: `/${collection}/${version}/usage/limitations` },
+				],
+			}
+		]
+	},
+	{
+		version: "v1",
+		content: []
+	}
+].find(v => v.version === version)?.content ?? []
+
+const pluginTypes = z.enum(["method-handler", "method-plugin", "callback-handler", "callback-plugin", "json-handler"])
+export type PluginType = z.infer<typeof pluginTypes>
+
+export type DocumentationCollection = typeof documentationCollections[number]
+export type CollectionType<C extends DocumentationCollection> = Awaited<ReturnType<typeof getCollection<C>>>[number]
 
 export const collections = {
-	docs: documentationCollection,
-	plugins: pluginsCollection,
-	api: apiCollection,
+	docs: defineCollection({
+		type: "content",
+		schema: z.object({
+			title: z.string(),
+			order: z.number().optional(),
+			description: z.string().optional(),
+		}),
+	}),
+	plugins: defineCollection({
+		type: "content",
+		schema: z.object({
+			title: z.string(),
+			order: z.number().optional(),
+			icon: z.string().optional(),
+			type: pluginTypes,
+			transport: z.enum(["http", "ws"]),
+			features: z.array(z.string()).optional(),
+		}),
+	}),
+	api: defineCollection({
+		type: "data",
+	})
 }
