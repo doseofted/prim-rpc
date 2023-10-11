daebea1dafbdf37e473592ec11ed0836ec171831
Ted Klingenberg <ted@doseofted.com>
2023-10-11 12:12:38 -0400

    Logo is now easier to see when effects are disabled

--- apps/documentation/src/utils/disable-effects.ts
+++ apps/documentation/src/utils/disable-effects.ts
@@ -2,9 +2,19 @@ export const visualEffects = {
 	get enabled() {
 		const effectsDisabled = sessionStorage.getItem("disable-effects")
 		const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches
-		return !(effectsDisabled || prefersReducedMotion)
+		const enabled = !(effectsDisabled || prefersReducedMotion)
+		if (enabled) {
+			document.body.classList.remove("disable-effects")
+		} else {
+			document.body.classList.add("disable-effects")
+		}
+		return enabled
 	},
 	get disabled() {
 		return !this.enabled
 	},
 }
+
+document.addEventListener("astro:after-swap", () => {
+	visualEffects.enabled
+})
